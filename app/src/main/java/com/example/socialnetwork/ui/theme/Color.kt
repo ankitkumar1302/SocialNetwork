2a5920076c87f805aaf4e88aedec7df28758e6a2
ankit <you@example.com>
2023-03-24 13:50:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/socialnetwork/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.socialnetwork.ui.theme
+
+import android.hardware.lights.Light
+import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.text.style.TextDrawStyle.Unspecified.color
+
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF404040)
+val LightGray = Color(0xFF606060)
+val HintGray = Color(0xFF6D6D6D)
+val TextWhite = Color(0xFFEEEEEE)
+val GreenAccent = Color(0xFF08FF04)
